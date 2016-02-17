04941d507fd0e82d6e9ad8dd2f1f47c38056ca50
Milan Grubnić <MilanG@aol.com>
2016-02-16 20:43:47 -0600

    no change

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-'use strict';
+	'use strict';
 
 
 // Declare app level module which depends on filters, and services
@@ -14,3 +14,4 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+	
