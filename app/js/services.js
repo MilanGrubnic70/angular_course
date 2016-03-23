f77c2268fa9b8bebcbac213ac981147a1cd35272
Milan Grubnić <MilanG@aol.com>
2016-03-22 19:23:32 -0500

    replaced using value with factory to make service

--- app/js/services.js
+++ app/js/services.js
@@ -3,4 +3,8 @@
 /* Services */
 
 angular.module('myApp.services', [])
-    .value('FIREBASE_URL', 'https://waitandeat-demo-ver1.firebaseio.com/');
+    // .value('FIREBASE_URL', 'https://waitandeat-demo-ver1.firebaseio.com/');
+    // Preferred way to make a service
+    .factory('FIREBASE_URL', function () {
+    	return 'https://waitandeat-demo-ver1.firebaseio.com/';
+    });
