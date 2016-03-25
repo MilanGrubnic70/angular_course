22317c276ba5c102a1c33dbc484a23629ba61559
Milan Grubnić <MilanG@aol.com>
2016-03-24 21:39:13 -0500

    created authService in services.js

--- app/js/services.js
+++ app/js/services.js
@@ -5,6 +5,21 @@
 angular.module('myApp.services', [])
     // .value('FIREBASE_URL', 'https://waitandeat-demo-ver1.firebaseio.com/');
     // Preferred way to make a service
-    .factory('FIREBASE_URL', function () {
-    	return 'https://waitandeat-demo-ver1.firebaseio.com/';
+    .factory('FIREBASE_URL', function() {
+        return 'https://waitandeat-demo-ver1.firebaseio.com/';
     });
+
+	.factory('authService', function($firebaseSimpleLogin, $location, FIREBASE_URL) {
+		var authRef = new Firebase(FIREBASE_URL);
+    	var auth = $firebaseSimpleLogin(authRef);
+
+        return {
+            login: function(user) {
+                auth.$login('password', user)
+                    .then(function(data) {
+                            console.log(data);
+                            $location.path('/waitlist');
+                        });
+                    };
+                };
+        });
