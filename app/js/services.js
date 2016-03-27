86df8d9332a08ebf2f3363f879d2bf70fc7603ac
Milan Grubnić <MilanG@aol.com>
2016-03-27 14:36:27 -0500

    refactored party service and injected data service

--- app/js/services.js
+++ app/js/services.js
@@ -13,14 +13,13 @@ angular.module('myApp.services', [])
 	.factory('dataService', function ($firebase, FIREBASE_URL) {
 		var dataRef =  new Firebase(FIREBASE_URL);
 		var fireData = $firebase(dataRef);
-		
+
 		return fireData;
 	})
 
 
-	.factory('partyService', function($firebase, FIREBASE_URL){
-		var partiesRef = new Firebase(FIREBASE_URL + 'parties');
-    	var parties = $firebase(partiesRef);
+	.factory('partyService', function(dataService){
+    	var parties = dataService.$child('parties');
 
     	var partyServiceObject = {
     		parties: parties,
