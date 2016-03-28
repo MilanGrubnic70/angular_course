3156db758c8a6ade72746e29c37c885fb6613587
Milan Grubnić <MilanG@aol.com>
2016-03-27 20:39:21 -0500

    created get parties by id method

--- app/js/services.js
+++ app/js/services.js
@@ -20,14 +20,15 @@ angular.module('myApp.services', [])
 
 	.factory('partyService', function(dataService){
 
-    	var parties = dataService.$child('parties');
     	var users = dataService.$child('users');
 
     	var partyServiceObject = {
-    		parties: parties,
     		saveParty: function (party, userId) {
 				parties.$add(party);
 				users.$child(userId).$child('parties').$add(party);
+    		},
+    		getPartiesByUserId: function (userId) {
+    			return users.$child(userId).$child('parties');
     		}
     	};
     	return partyServiceObject;
